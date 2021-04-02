8cb952d5c302d02a791cd95ead2f93ac0e88f5aa
Morghan <morghan102@gmail.com>
2021-04-01 17:18:15 -0700

    Lifted non-functional state up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Products from './components/Products';
 // import data from "./data.json";
 
@@ -11,6 +11,34 @@ import Products from './components/Products';
 // THIS WORKS, W/O IMPORTING AND W HEADERS: {}
 function App() {  
 
+// lifting state up for the products
+  const [products, setProducts]=useState([]);
+  // usestate() takes the initial state as an argument and returns an array with two items. The first item is a variable containing the state, which you will often use in your JSX. The second item in the array is a function that will update the state.
+
+  // i gave up on the json data AGGGGHHHHHAGSHGJGDJAS
+  const getData=()=>{
+    fetch('data.json'
+    ,
+    {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setProducts(myJson);
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <div className="grid-contatiner">
       <header>
@@ -19,7 +47,7 @@ function App() {
       <main>
         <div className="content">
           <div className="main">
-            <Products />
+            <Products products={products} />
           </div>
           <div className="sidebar">Cart Items</div>
         </div>
